cac15b5a1dcb3486a9a582426ddd68a45be4b86f
P1ST-0N <luckylionia@gmail.com>
2023-06-23 20:06:59 +0300

    added store.js in redux

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 
-// import { store, persistor } from "redux/store";
+import { store, persistor } from "redux/store";
 import App from "components/App/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename="react-hw-08-phonebook">
-      {/* <Provider store={store}> */}
-      <Provider>
-        {/* <PersistGate loading={null} persistor={persistor}> */}
-        <PersistGate loading={null}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
       </Provider>
