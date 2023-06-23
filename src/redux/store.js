ad1aaa9fdee556d217c5ebf6c478b00fce378b5d
P1ST-0N <luckylionia@gmail.com>
2023-06-23 16:44:10 +0300

    added userApi in redux

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+import { userApi } from "./userApi";
+//   import
