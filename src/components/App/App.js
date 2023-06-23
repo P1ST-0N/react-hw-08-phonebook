cd020a3dffe83210b34b5d48e6e232bbff32d662
P1ST-0N <luckylionia@gmail.com>
2023-06-23 16:07:20 +0300

    create & prepare new App

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { useSelector } from "react-redux";
+
+
+function App() {
+  return (
+    <div className="App">
+      dsfs
+    </div>
+  );
+}
+
+export default App;
