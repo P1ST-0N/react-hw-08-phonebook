7f92cdd48592a810557672bb099b0fb5bf5301a5
P1ST-0N <luckylionia@gmail.com>
2023-06-23 16:51:27 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import { useSelector } from "react-redux";
+import { userApi } from "redux/userApi";
 
+import { getToken } from "redux/selectors";
+import { useGetCurrentQuery } from "redux/userApi";
 
-function App() {
-  return (
-    <div className="App">
-      dsfs
-    </div>
-  );
-}
+import css from "./App.module.css";
+
+const App = () => {
+  const token = useSelector(getToken);
+
+  const { isLoading } = useGetCurrentQuery(undefined, {
+    skip: !token,
+  });
+
+  return <div className="App">dsfs</div>;
+};
 
 export default App;
